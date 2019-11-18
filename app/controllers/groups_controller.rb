e3a64513d1976b59bef318c6d96e9266f9909377
belzunces <lcbelzunces@gmail.com>
2019-11-18 17:58:11 +0100

    k

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,6 @@
 class GroupsController < ApplicationController
-<<<<<<< HEAD
   skip_before_action :authenticate_user!, only: [:create]
 
-=======
-  skip_before_action :authenticate_user!, only: [:home, :index, :categories, :show]
->>>>>>> bd6279c0b778336e0c34396d4964a99198edc9e5
   def index
   end
 
@@ -17,16 +13,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new
-<<<<<<< HEAD
     @group.save
-=======
-    # creator = @user.id
-    if @group.save
-      redirect_to group_path(@group)
-    else
-      render :new
-    end
->>>>>>> bd6279c0b778336e0c34396d4964a99198edc9e5
   end
 
   def update
