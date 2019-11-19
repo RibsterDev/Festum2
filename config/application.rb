c4f5e536c836277217a563946df3e2021b5c83f4
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-19 12:09:00 +0100

    scrapping de batard avec evan

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Festum2
   class Application < Rails::Application
     config.generators do |generate|
@@ -15,7 +17,8 @@ module Festum2
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+  config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+  config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
