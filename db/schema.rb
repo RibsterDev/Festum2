011d7e1df3c9a1ed3f043505be9f200f5c9662d1
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-19 15:39:29 +0100

    creation group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_134236) do
+ActiveRecord::Schema.define(version: 2019_11_19_143804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_134236) do
     t.integer "proposition_duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "user_groups", force: :cascade do |t|
@@ -64,12 +65,12 @@ ActiveRecord::Schema.define(version: 2019_11_18_134236) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
