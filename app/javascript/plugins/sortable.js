fc18d944dbeb8e94e41a4f3afa7e925b0ca550af
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-22 16:47:43 +0100

    Fix JS errors

--- app/javascript/plugins/sortable.js
+++ app/javascript/plugins/sortable.js
@@ -1,9 +1,11 @@
 import Sortable from 'sortablejs';
 
 const initSortable = () => {
-  console.log("test")
   const list = document.querySelector('.list_event')
-  Sortable.create(list);
+
+  if (list) {
+    Sortable.create(list);
+  }
   // sortable.option("enable");
 };
 
@@ -12,11 +14,13 @@ console.log("ertyh");
 
 
 const lea = document.querySelector('.valid-vote');
-lea.addEventListener("click", (event) => {
-  event.preventDefault()
-  const lis = document.querySelector(".list_event").getElementsByTagName("li");
-  console.log(lis)
-});
+if (lea) {
+  lea.addEventListener("click", (event) => {
+    event.preventDefault()
+    const lis = document.querySelector(".list_event").getElementsByTagName("li");
+    console.log(lis)
+  });
+}
 // console.log(lis);
 // var lis = document.getElementById(".list_event").getElementsByTagName("li");
 // var order = sortable.toArray();
