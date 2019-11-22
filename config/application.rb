0b58ccd5eceb9347c3db9cb9b4b5f141e4489457
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-22 15:25:42 +0100

    sortable

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Festum2
   config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :en
   config.active_job.queue_adapter = :sidekiq
+  config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
