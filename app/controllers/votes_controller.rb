ade82cbb155ce39013505d6a95b3e1e777784375
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-25 16:11:10 +0100

    dragndrop fix

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,19 @@
 class VotesController < ApplicationController
 
-  def index
+  def new
     @group = Group.find(params[:group_id])
+  end
 
+  def create
+
+    @result_total.nil? ? @result_total = {} : ""
+    @result = params.require(:votes).map { |d| d[:event_user_id] }
+    @result.each_with_index do |score_par_event, index|
+      @result_total[score_par_event].nil? ? @result_total[score_par_event] = index : @result_total[score_par_event] = @result_total[score_par_event] + index
+    end
+    params[:result_total] = @result_total
+    # rails.raise
+    redirect_to groups_path(params[:group_id], @result_total)
   end
 
   # def score
