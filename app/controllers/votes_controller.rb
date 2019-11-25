c7028085e1591de91463b6794b46f3fd6210c369
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-25 18:49:14 +0100

    point pour les votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,21 +5,36 @@ class VotesController < ApplicationController
   end
 
   def create
-
-    @result_total.nil? ? @result_total = {} : ""
-    @result = params.require(:votes).map { |d| d[:event_user_id] }
-    @result.each_with_index do |score_par_event, index|
-      @result_total[score_par_event].nil? ? @result_total[score_par_event] = index : @result_total[score_par_event] = @result_total[score_par_event] + index
+    # @vote = Vote.new(votes_params)
+    params[:votes].each_with_index do |vote_data, index|
+      Vote.create!(
+        user: current_user,
+        event_user_id: vote_data[:event_user_id],
+        sort: index
+      )
     end
-    params[:result_total] = @result_total
-    # rails.raise
-    redirect_to groups_path(params[:group_id], @result_total)
+
+
+    # @result_total.nil? ? @result_total = {} : ""
+    # @result = params.require(:votes).map { |d| d[:event_user_id] }
+    # @result.each_with_index do |score_par_event, index|
+    #   @result_total[score_par_event].nil? ? @result_total[score_par_event] = index : @result_total[score_par_event] = @result_total[score_par_event] + index
+    # end
+    # # @result_total = vote_id[@vote.user_id] = @result_total
+    # params[:result_total] = @result_total
+    # raise
+    redirect_to group_path(params[:group_id]), notice: 'A voté !'
   end
 
   # def score
   #   score_total << params[:votes_result]
   # end
 
+  private
+
+  def votes_params
+    params.require(:votes).permit(:user_id, :event_user_id)
+  end
 
 # cree vote new et vote save
 end
