cecb65fa8dc548029391f52341342694a14b8251
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-26 10:23:59 +0100

    fixe bug name

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_11_24_172848) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
