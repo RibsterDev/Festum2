c77869d39d6564cf748ad4b8943388d2c559a0a4
Nicolas Morlot <nicolasmorlot@gmail.com>
2020-02-05 13:44:37 +0100

    change url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "https://festumm.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://git.heroku.com/festumm.git" }
 
   # config.action_mailer.default_url_options = { host: "http://festeam.io" }
   # Settings specified here will take precedence over those in config/application.rb.
