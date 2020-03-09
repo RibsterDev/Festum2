d5eb75c6345fdafea40aa0e1430ffb1349304545
Nicolas Morlot <nicolasmorlot@gmail.com>
2020-03-09 18:00:42 +0100

    error solved

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  # resources :users do
+   # resources :recruiters, only: [:new, :create]
+   # end
+
   resources :events, only: [:index, :show] do
      collection do
       get 'categories'
